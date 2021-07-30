01fb524c1ba17bd99a99f03923bd7b61782a360f
xoo2001 <ekora2001@gmail.com>
2021-07-30 07:33:59 +0000

    wt88047x: build selinux permissive

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -67,6 +67,7 @@ BOARD_KERNEL_IMAGE_NAME := zImage-dtb
 TARGET_KERNEL_SOURCE := kernel/wingtech/msm8916
 TARGET_KERNEL_CONFIG := lineageos_wt88047_defconfig
 BOARD_KERNEL_CMDLINE += phy-msm-usb.floated_charger_enable=1
+BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 
 # Recovery
 TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/etc/fstab.qcom
