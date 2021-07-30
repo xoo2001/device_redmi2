1467a421fcee45a22f85aa698c5688dfd9c06411
xoo2001 <ekora2001@gmail.com>
2021-07-30 07:40:18 +0000

    wt88047x: enable blur by default

--- system_prop.mk
+++ system_prop.mk
@@ -5,6 +5,12 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.vendor.audio.fluence.voicerec=false \
     persist.vendor.audio.fluence.speaker=false
 
+# Blur
+ro.sf.blurs_are_expensive=1
+ro.surface_flinger.supports_background_blur=1
+debug.sf.latch_unsignaled=1
+debug.sf.disable_backpressure=1
+
 # Camera
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.camera.cpp.duplication=false \
