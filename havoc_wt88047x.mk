b77c5f188b6afc37bbb498a5ea68a19057b012a3
xoo2001 <ekora2001@gmail.com>
2021-08-06 08:16:32 +0000

    wt88047x: havoc-ify

--- havoc_wt88047x.mk
+++ havoc_wt88047x.mk
@@ -16,7 +16,9 @@
 $(call inherit-product, device/xiaomi/wt88047x/full_wt88047x.mk)
 
 # Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_mini_phone.mk)
+$(call inherit-product, vendor/havoc/config/common_mini_phone.mk)
+TARGET_BOOT_ANIMATION_RES := 720
+WITH_GAPPS := false
 
 # Shipping API level
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_k.mk)
@@ -27,7 +29,7 @@ TARGET_BOARD_PLATFORM_VARIANT := msm8916
 # Assert
 TARGET_OTA_ASSERT_DEVICE := HM2014811,HM2014812,HM2014813,HM2014814,HM2014815,HM2014816,HM2014817,HM2014818,HM2014819,HM2014820,HM2014821,HM2014112,wt88047,wt86047,wt88047x
 
-PRODUCT_NAME := lineage_wt88047x
+PRODUCT_NAME := havoc_wt88047x
 BOARD_VENDOR := xiaomi
 PRODUCT_DEVICE := wt88047x
 
